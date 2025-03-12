68b41cddf275027cdcf428e045d4c3676fdcefed
perla-beep <perla.desantiago@mail.citytech.cuny.edu>
2025-03-12 13:47:48 -0400

    Update sketch.js

--- SS4_PerlaD/sketch.js
+++ SS4_PerlaD/sketch.js
@@ -1,19 +1,36 @@
-//Perla DS Chicken attempt
+//Perla DS "Chicken attempt" I find chicken very cute
+// So i wanted to include it in my sketch
 let chicken;
+let cTime = 0;
+let t1=3000;
+
 
 function preload()
 {
   chicken= loadImage("images/chicken.png");
+  egg= loadImage("images/egg.png")
 }
 
 function setup(){
   createCanvas(800,800);
-  background(245, 120, 203);
+  background(254,228,169);
   imageMode(CENTER);
 }
 
 function draw(){
-  background(245, 120, 203);
-  text("CUTE CHICKEN",0,200,200,400);
+  cTime=millis();
+
+  background(254,228,169);
+  textSize(60)
+  textAlign(CENTER)
+  text("CUTE CHICK",0,100,800,200);
+  image(chicken, width/2,height/2);
+
+  if(cTime>t1){
+    background(76,196,210)
+    image(egg, width/2,height/2)
+    text("mmmm yummy egg",0,100,800,200)
+  }
+
 
 }
