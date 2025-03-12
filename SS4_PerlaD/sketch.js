a7d1b421aa82d057c27657f30cfbc772bdef1d92
perla-beep <perla.desantiago@mail.citytech.cuny.edu>
2025-03-12 08:45:16 -0400

    Add files via upload

---
+++ SS4_PerlaD/sketch.js
@@ -0,0 +1,19 @@
+//Perla DS Chicken attempt
+let chicken;
+
+function preload()
+{
+  chicken= loadImage("images/chicken.png");
+}
+
+function setup(){
+  createCanvas(800,800);
+  background(245, 120, 203);
+  imageMode(CENTER);
+}
+
+function draw(){
+  background(245, 120, 203);
+  text("CUTE CHICKEN",0,200,200,400);
+
+}
